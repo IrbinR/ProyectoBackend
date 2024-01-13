b31dcc12e7961a9cea8120d4724c06b2bc979946
IrbinR <salazar.irbin@gmail.com>
2024-01-13 00:36:01 -0500

    Inicio del proyecto

---
+++ GaleriaArte/src/test/java/com/codigo/GaleriaArte/GaleriaArteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codigo.GaleriaArte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaleriaArteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
